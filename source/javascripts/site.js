468e003c1570a4123682d9cf2f1ee0cf2b3dbf54
Hugo Payet <hugopayetsync@gmail.com>
2019-02-10 18:35:37 +0100

    initial commit, middleman + unify

---
+++ source/javascripts/site.js
@@ -0,0 +1,37 @@
+// This is where it all goes :)
+
+
+// ---- JS Global Compulsory ----
+//= require jquery/jquery.min
+//= require jquery-migrate/jquery-migrate.min
+//= require popper.min
+//= require bootstrap/bootstrap.min
+//= require bootstrap/offcanvas
+//= require jquery.easing/js/jquery.easing.min
+
+// ---- JS Implementing Plugins ----
+//= require hs-megamenu/src/hs.megamenu
+//= require dzsparallaxer/dzsparallaxer
+//= require dzsparallaxer/dzsscroller/scroller
+//= require dzsparallaxer/advancedscroller/plugin
+//= require chosen/chosen.jquery
+//= require image-select/src/ImageSelect.jquery
+//= require masonry/dist/masonry.pkgd.min
+//= require imagesloaded/imagesloaded
+//= require slick-carousel/slick/slick
+//= require malihu-scrollbar/jquery.mCustomScrollbar.concat.min
+//= require fancybox/jquery.fancybox.min
+//= require jquery.countdown.min
+//= require jquery.maskedinput/src/jquery.maskedinput
+//= require appear
+//= require gmaps/gmaps.min
+
+
+
+
+//=require unify/offcanvas
+//=require unify/hs.core
+//=require unify/hs.header
+//=require unify/hs.hamburgers
+//=require unify/hs.popup
+//=require unify/hs.go-to
